338efabdbd20665fe2fff5db1a0b1ab8684e682e
NeuronByte <parav.jaikrishan@gmail.com>
2021-07-05 16:31:16 +0000

    Challenge abc182_a submitted

---
+++ atcoder/abc182_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int a, b;
+    cin >> a >> b;
+    cout << 2*a + 100 - b;
+    return 0;
+}
