a0e4c7f4861d0ab22c3ae5ad9a4c3e1855817055
Neon Byte <77317703+NeuronByte@users.noreply.github.com>
2021-07-15 22:27:42 +0530

    Update challenge abc186_a

--- atcoder/abc186_a.cpp
+++ atcoder/abc186_a.cpp
@@ -2,10 +2,10 @@
 using namespace std;
  
 int main() {
-  	ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-  	int n, w;
-    cin >> n >> w;
-    cout << n/w << endl;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n, w;
+	cin >> n >> w;
+	cout << n/w << endl;
 }
